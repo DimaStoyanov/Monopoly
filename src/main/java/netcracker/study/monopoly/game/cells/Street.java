6641bd4b38fb6c87e9bd9b64f5110abce9e52cf6
Kest <k.sharipo@mail.ru>
2018-03-21 13:04:43 +0300

    new street

--- src/main/java/netcracker/study/monopoly/game/cells/Street.java
+++ src/main/java/netcracker/study/monopoly/game/cells/Street.java
@@ -1,14 +1,12 @@
 package netcracker.study.monopoly.game.cells;
 
 import netcracker.study.monopoly.game.Gamer;
-import netcracker.study.monopoly.game.resources.Own;
 
 public class Street implements Cell{
 
     int cost;
     Gamer owner;
     String Name;
-    Own own;
     boolean toBuy;
     int position;
 
